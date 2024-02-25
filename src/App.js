409b29866d4e3d899a1ec6137efd73d3a88b28e3
Inamul Haq <ihaq81285@gmail.com>
2024-02-25 23:31:27 +0530

    calculator app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const btnValues = [
+    [7, 8, 9, "+"],
+    [4, 5, 6, "-"],
+    [1, 2, 3, "*"],
+    ["C", 0, "=", "/"],
+  ];
+
+  const [inputValue, setInputValue] = useState("");
+  const [result, setResult] = useState("");
+
+  const handleButtonClick = (value) => {
+    if (value === "=") {
+      const res = eval(inputValue);
+      if (inputValue == 0 / 0) {
+        setResult("NaN");
+      } else if (inputValue == "") {
+        setResult("Error");
+      } else if (res == Infinity) {
+        setResult("Infinity");
+      } else {
+        setResult(res);
+      }
+
+      setInputValue("");
+    } else if (value === "C") {
+      setInputValue("");
+      setResult("");
+    } else {
+      setInputValue((prevInputValue) => prevInputValue + value);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Calculator</h1>
+      <input
+        style={{ width: "18em", height: "1.5em" }}
+        type="text"
+        value={inputValue}
+        readOnly
+      />
+      <div style={{ padding: "1em" }}>{result}</div>
+      {btnValues.map((buttonRow, i) => {
+        return (
+          <div key={i} className="row">
+            {buttonRow.map((buttonValue, j) => {
+              return (
+                <button
+                  key={j}
+                  onClick={() => handleButtonClick(buttonValue)}
+                  className="btn"
+                >
+                  {buttonValue}
+                </button>
+              );
+            })}
+          </div>
+        );
+      })}
     </div>
   );
 }
-
-export default App;
